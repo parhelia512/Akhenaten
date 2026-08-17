1db636cfc88e303366282c05e2ec61a05835ed8d
s.kushnirenko <sergey.kushnirenko@gaijin.team>
2026-08-17 18:02:56 +0200

    Migrate tax collector overlay and mothball to ES handlers.

--- src/scripts/ui_tax_collector_window.js
+++ src/scripts/ui_tax_collector_window.js
@@ -26,18 +26,26 @@ taxcollector_info_window {
         show_overlay  : button({
                                 margin:{right:-64, bottom:-40}, size:[23, 23]
                                 textfn: building_info_window_text_overlay
-                                onclick: building_info_window_toggle_overlay
                                })
 
         mothball      : button({
                                 margin:{right:-90, bottom:-40}, size:[23, 23]
                                 textfn: building_info_window_text_mothball
-                                onclick: building_info_window_toggle_mothball
                                })
 
         button_help   : help_button({}),
         button_close  : close_button({}),
     }
+}
+
+[es=(taxcollector_info_window, show_overlay)]
+function taxcollector_info_window_on_show_overlay(window) {
+    building_info_window_toggle_overlay()
+}
+
+[es=(taxcollector_info_window, mothball)]
+function taxcollector_info_window_on_mothball(window) {
+    building_info_window_toggle_mothball()
 }
 
 [es=(taxcollector_info_window, init)]
